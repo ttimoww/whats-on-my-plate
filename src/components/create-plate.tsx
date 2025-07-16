f2a0e0b726e956ed6563cace83624228396a70b2
Timo <ttimoww@gmail.com>
2025-07-16 21:22:10 +0200

    Add new dependencies and implement image upload functionality
    - Added `@radix-ui/react-avatar` and `react-dropzone` as new dependencies in package.json.
    - Introduced a new UploadImage component for handling image uploads in the CreatePlate component.
    - Refactored the CreatePlate component to utilize the new UploadImage component for file input.
    - Created a new Avatar component for displaying uploaded images with fallback options.
    - Implemented a Dropzone UI for improved user experience during image uploads.

--- src/components/create-plate.tsx
+++ src/components/create-plate.tsx
@@ -21,6 +21,15 @@ import { cn, convertFileToBase64 } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { LoadingOverlay } from "@/components/ui/loading-overlay";
 import { useUrlState } from "@/hooks/use-url-state";
+import {
+  Dropzone,
+  DropZoneArea,
+  DropzoneTrigger,
+  useDropzone,
+} from "@/components/ui/dropzone";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ImageIcon } from "lucide-react";
+
 const formSchema = z.object({
   description: z.string().optional(),
   image: z.instanceof(File, { message: "A photo of your plate is required" }),
@@ -67,15 +76,7 @@ export function CreatePlate({ className, ...props }: CreatePlateProps) {
                 <FormItem>
                   <FormLabel>Your plate</FormLabel>
                   <FormControl>
-                    <Input
-                      type="file"
-                      accept="image/*"
-                      onChange={(e) => {
-                        const file = e.target.files?.[0];
-                        onChange(file);
-                      }}
-                      {...field}
-                    />
+                    <UploadImage onChange={onChange} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -88,7 +89,7 @@ export function CreatePlate({ className, ...props }: CreatePlateProps) {
                 <FormItem>
                   <FormLabel>Description (optional)</FormLabel>
                   <FormControl>
-                    <Textarea placeholder="Enter description" {...field} />
+                    <Textarea {...field} className="resize-none" />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -101,5 +102,51 @@ export function CreatePlate({ className, ...props }: CreatePlateProps) {
         </Form>
       </CardContent>
     </Card>
+  );
+}
+
+interface UploadImageProps {
+  onChange: (file: File | undefined) => void;
+}
+function UploadImage({ onChange }: UploadImageProps) {
+  const dropzone = useDropzone({
+    onDropFile: async (file: File) => {
+      onChange(file);
+      return {
+        status: "success",
+        result: URL.createObjectURL(file),
+      };
+    },
+    validation: {
+      accept: {
+        "image/*": [".png", ".jpg", ".jpeg"],
+      },
+      maxFiles: 1,
+    },
+    shiftOnMaxFiles: true,
+  });
+
+  const avatarSrc = dropzone.fileStatuses[0]?.result;
+  const isPending = dropzone.fileStatuses[0]?.status === "pending";
+
+  return (
+    <Dropzone {...dropzone}>
+      <DropZoneArea>
+        <DropzoneTrigger className="flex items-center gap-4 bg-transparent text-sm">
+          <Avatar className={cn("size-12", isPending && "animate-pulse")}>
+            <AvatarImage className="object-cover" src={avatarSrc} />
+            <AvatarFallback>
+              <ImageIcon className="size-4" />
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col gap-1">
+            <p>Upload a photo of your plate</p>
+            <p className="text-muted-foreground text-xs">
+              Drag and drop or click to upload
+            </p>
+          </div>
+        </DropzoneTrigger>
+      </DropZoneArea>
+    </Dropzone>
   );
 }
